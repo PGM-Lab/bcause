b6a9aa5b22878ce8012e38b924bad0bb93a355d8
Rafael Cabañas <rcabanas@ual.es>
2023-10-05 14:58:34 +0200

    fixed bug scmwrite

--- bcause/readwrite/bnwrite.py
+++ bcause/readwrite/bnwrite.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from pgmpy.readwrite import BIFWriter, XMLBIFWriter
 
-from bcause.factors import MultinomialFactor
+from bcause.factors import MultinomialFactor, DeterministicFactor
 from bcause.util.assertions import assert_file_exists
 
 
@@ -32,33 +32,38 @@ def to_uai(model:'BayesianNetwork', filepath, reverse_values=False, label="BAYES
 
     integer_varlist = integer_varlist or []
 
-    print(reverse_values)
+    variables = model.endogenous + model.exogenous if label == "CAUSAL" else model.variables
+
 
     out = f"{label}\n"
-    out += f"{len(model.variables)}\n"
-    out += " ".join([str(i) for i in model.varsizes.values()])
+    out += f"{len(variables)}\n"
+    out += " ".join([f"{model.varsizes[v]}" for v in variables])
     out += "\n"
 
-    var_idx = {v: model.variables.index(v) for v in model.variables}
+    var_idx = {v: variables.index(v) for v in variables}
     out += f"{len(model.factors)}\n"
 
-    for v in model.variables:
-        idx = [var_idx[x] for x in model.get_parents(v) + [v]]
+    var_order = dict()
+
+    for v in variables:
+        var_order[v] = [x for x in model.get_parents(v)][::-1] + [v]
+        idx = [var_idx[x] for x in var_order[v]]
         out += f"{len(idx)}\t" + " ".join([str(p) for p in idx]) + "\n"
 
     out += "\n"
 
-    for v in model.variables:
+    for v in variables:
         f = model.factors[v]
-        var_order = f.variables
+        vorder = var_order[v]
 
-        if label=="CAUSAL" and v in model.endogenous and isinstance(f, MultinomialFactor):
-            f = f.to_deterministic()
-            var_order = [x for x in var_order if x != v]
+        if label=="CAUSAL" and v in model.endogenous:
+            if not isinstance(f, DeterministicFactor):
+                f = f.to_deterministic()
+            vorder = [x for x in vorder if x != v]
 
-        if not reverse_values:
-            var_order = var_order[::-1]
-        values = f.values_array(var_order).flatten()
+        # if not reverse_values:
+        #     var_order = var_order[::-1]
+        values = f.values_array(vorder).flatten()
         if v in integer_varlist:
             values = [int(p) for p in values]
         out += f"{len(values)}\t" + " ".join([str(p) for p in values]) + "\n"
