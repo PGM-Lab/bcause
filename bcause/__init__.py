a21186f019b71e2d9268f692c5f6c8ce5daed566
Rafael Cabañas <rcabanas@ual.es>
2024-01-19 18:25:57 +0100

    problem imports

--- bcause/__init__.py
+++ bcause/__init__.py
@@ -4,5 +4,7 @@ __version__ = '0.0.1'
 #from . import factors
 #from . import util
 
-from .util import randomUtil as randomUtil
+
+import .bcause.util.randomUtil as randomUtil
+#from .util import randomUtil as randomUtil
 
