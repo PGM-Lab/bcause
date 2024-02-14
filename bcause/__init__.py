e2dd00a188c216a1ae5ddc41e8966acf9ac7dc3e
Rafael Cabañas <rcabanas@ual.es>
2024-02-14 12:09:09 +0100

    package fix

--- bcause/__init__.py
+++ bcause/__init__.py
@@ -1,12 +1,12 @@
 __version__ = '0.0.1'
 
-from .conversion import *
+#from .conversion import *
 # from .factors import *
 # from .inference import *
 # from .learning import *
 # from .models import *
 # from .readwrite import *
-# from .util import *
+from .util import *
 
 #from . import inference
 #from . import learning
