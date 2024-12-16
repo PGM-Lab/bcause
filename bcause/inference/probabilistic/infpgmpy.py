f985768e9e86a0391b2ae5ac8d30398748738a59
Rafael Cabañas <rcabanas@ual.es>
2024-12-16 11:36:40 +0100

    elim order VEPGMPY

--- bcause/inference/probabilistic/infpgmpy.py
+++ bcause/inference/probabilistic/infpgmpy.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from bcause.conversion.pgmpy import toPgmpyBNet, discrete_to_multinomial
 from bcause.factors.factor import Factor
 from bcause.inference.probabilistic import ProbabilisticInference
@@ -9,17 +11,24 @@ class VariableEliminationPGMPY(ProbabilisticInference):
     from the pgmpy library, adapted to work with bcause's probabilistic model framework.
     """
 
-    def __init__(self, model: PGModel):
+    def __init__(self, model: PGModel, elimination_order:Union[str,list]="minfill"):
         """
         Initializes the Variable Elimination inference engine.
 
         Args:
             model (PGModel): The probabilistic graphical model to be used for inference.
+
+            elimination_order: str or list (default='minfill') Order in which to eliminate the variables in the algorithm. If list is provided,
+            should contain all variables in the model except the ones in `variables`. str options
+            are:  `WeightedMinFill`, `MinNeighbors`, `MinWeight`, `MinFill`. Please
+            refer https://pgmpy.org/exact_infer/ve.html#module-pgmpy.inference.EliminationOrder
+            for details.
         """
         self._model = model
         self._evidence = dict()
         self._target = None
         self._compiled = False
+        self._elimination_order = elimination_order
 
         from pgmpy.inference import VariableElimination
         self._inf = VariableElimination(toPgmpyBNet(self._model))
@@ -32,7 +41,7 @@ class VariableEliminationPGMPY(ProbabilisticInference):
         Returns:
             Factor: The resulting probability distribution as a bcause-compatible Factor object.
         """
-        p = self._inf.query(self._target, self._evidence)
+        p = self._inf.query(self._target, self._evidence, show_progress=False, elimination_order=self._elimination_order)
         return discrete_to_multinomial(p, left_vars=self._target)
 
 
