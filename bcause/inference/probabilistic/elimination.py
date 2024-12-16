f150592c753dceb9db9fa514b6175d280c6ed977
Rafael Cabañas <rcabanas@ual.es>
2024-12-16 16:22:34 +0100

    debug messages VE

--- bcause/inference/probabilistic/elimination.py
+++ bcause/inference/probabilistic/elimination.py
@@ -73,17 +73,18 @@ class VariableElimination(ProbabilisticInference):
             if len(fnew.left_vars)>0:
                 factors += [fnew]
 
-            logging.getLogger( __name__ ).debug(f"Updated factor list: {[f.name for f in factors]}")
+                logging.getLogger(__name__).debug(f"Updated factor list: {[f.name for f in factors]}")
 
+            p = reduce((lambda f1, f2: f1 * f2), factors)
+            # combine resulting factors and set evidence
+            joint = reduce((lambda f1, f2: f1 * f2), factors).R(**self._evidence)
 
-
-        p = reduce((lambda f1, f2: f1 * f2), factors)
-        # combine resulting factors and set evidence
-        joint = reduce((lambda f1, f2: f1 * f2), factors).R(**self._evidence)
-
-
+            logging.getLogger(__name__).debug(f"Calculated joint probability {joint}")
 
         result = joint / (joint ** self._target)
+
+        logging.getLogger(__name__).debug(f"Result {result}")
+
         self.time = (time.time()-tstart)*1000
         logging.getLogger( __name__ ).info(f"Finished variable elimination in {self.time} ms.")
         return result
