c00b8aebcdc68cade20fd48ab078bd05485756ea
Rafael Cabañas <rcabanas@ual.es>
2026-02-02 15:46:01 +0100

    repeating experiments

--- papers/gradient_journal/code/experiments.sh
+++ papers/gradient_journal/code/experiments.sh
@@ -1,16 +1,11 @@
-#sbatch  --export=nparents=2,nzr=10 -n 100 experiments.sbs
-#sbatch  --export=nparents=2,nzr=08 -n 100 experiments.sbs
-#sbatch  --export=nparents=2,nzr=06 -n 100 experiments.sbs
-#sbatch  --export=nparents=2,nzr=04 -n 100 experiments.sbs
-#sbatch  --export=nparents=2,nzr=02 -n 100 experiments.sbs
 
-#sbatch  --export=nparents=3,nzr=10 -n 100 experiments.sbs
-#sbatch  --export=nparents=3,nzr=08 -n 100 experiments.sbs
-#sbatch  --export=nparents=3,nzr=06 -n 100 experiments.sbs
-#sbatch  --export=nparents=3,nzr=04 -n 100 experiments.sbs
-#sbatch  --export=nparents=3,nzr=02 -n 100 experiments.sbs
 
-sbatch  --export=nparents=1 -n 100 experiments.sbs
-sbatch  --export=nparents=3 -n 100 experiments.sbs
-sbatch  --export=nparents=2 -n 100 experiments.sbs
+sbatch  --export=method=EMCC_50 experiments.sbs
+sbatch  --export=method=EMCC_100 experiments.sbs
+sbatch  --export=method=EMCC_150 experiments.sbs
 
+sbatch  --export=method=GDCC_1e-05 experiments.sbs
+sbatch  --export=method=GDCC_1e-07 experiments.sbs
+sbatch  --export=method=GDCC_1e-09 experiments.sbs
+
+sbatch  --export=method=GSCC experiments.sbs
