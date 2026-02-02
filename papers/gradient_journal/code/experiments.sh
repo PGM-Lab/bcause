a835b6df038dba7def7641463a3aec09081d7c24
Rafael Cabañas <rcabanas@ual.es>
2026-02-02 16:02:33 +0100

    repeating experiments

--- papers/gradient_journal/code/experiments.sh
+++ papers/gradient_journal/code/experiments.sh
@@ -1,14 +1,14 @@
 
 
-METHOD=EMCC_50; sbatch  --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
-METHOD=EMCC_100; sbatch  --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
-METHOD=EMCC_150; sbatch  --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
+METHOD=EMCC_50; sbatch  --job-name=$METHOD --export=method=$METHOD experiments.sbs
+METHOD=EMCC_100; sbatch  --job-name=$METHOD --export=method=$METHOD experiments.sbs
+METHOD=EMCC_150; sbatch  --job-name=$METHOD --export=method=$METHOD experiments.sbs
 
 
-METHOD=GDCC_1e-05; sbatch --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
-METHOD=GDCC_1e-07; sbatch --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
-METHOD=GDCC_1e-09; sbatch --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
+METHOD=GDCC_1e-05; sbatch --job-name=$METHOD --export=method=$METHOD experiments.sbs
+METHOD=GDCC_1e-07; sbatch --job-name=$METHOD --export=method=$METHOD experiments.sbs
+METHOD=GDCC_1e-09; sbatch --job-name=$METHOD --export=method=$METHOD experiments.sbs
 
 
-METHOD=GSCC; sbatch --job-name=gd_$METHOD --export=method=$METHOD experiments.sbs
+METHOD=GSCC; sbatch --job-name=$METHOD --export=method=$METHOD experiments.sbs
 
