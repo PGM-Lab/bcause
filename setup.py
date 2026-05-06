c7980259cee5e182eda8efe36cf9c559dee1f570
Rafael Cabañas <rcabanas@ual.es>
2026-05-06 16:02:00 +0200

    updated pandas req

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ except IndexError:
 #REQUIREMENTS = [i.strip() for i in open("requirements/install.txt").readlines()]
 REQUIREMENTS = [
     "numpy~=1.26 ",
-    "pandas~=2.0.3 ",
+    "pandas~=2.2.0 ",
     "matplotlib~=3.5.2 ",
     "networkx~=2.6.3 ",
     "pgmpy~=0.1.17 ",
